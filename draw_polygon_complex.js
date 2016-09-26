ee78436921f307b4acf57c414518f8993d734794
Kelly Milligan <kelly@milligan.net.nz>
2016-09-26 16:31:39 +0200

    Update draw polygon 2 to complex revision
    Has fixed floating point errors, can properly plot short lengths, has a counter-clockwise flag to internally handle canvas reflection, and has general tidying

--- draw_polygon_complex.js
+++ draw_polygon_complex.js
@@ -1,12 +1,12 @@
 define([
 
-    '../math/point_on_circle',
-    '../math/lerp_point_2d'
+    '../math/point/point_on_circle',
+    '../math/point/point_lerp_2d'
 
 ], function (
 
     pointOnCircle,
-    lerpPoint2d
+    pointLerp2d
 
 ) {
 
@@ -14,49 +14,48 @@ define([
 
     /*
 
-        ** DRAFT **
-        - To add ability to animate counter-clockwise
-        - Can't animate very short length's without the indexes doing odd things
-        - General tidy up
-
-        Draw a symmetrical polygon on the provided canvas 2d context.
-        By default, draws a full, closed path polygon.
-        Starting angle and length can be passed to draw an open-ended polygon path.
+        Draw a symmetrical 2D polygon on the provided canvas 2d context.
+        ---
+        By default draw a full, closed path polygon with it's first point at North.
+        A starting angle and length can be provided to draw an open-ended polygon.
 
         ctx           context2D   Canvas context to draw to
-        x             num         X position at centre of circle
-        y             num         Y position at centre of circle
-        r             num         Radius of circle
+        x             num         X position at centre of polygon
+        y             num         Y position at centre of polygon
+        r             num         Radius of polygon
         n             num         Number of points that form the polygon, minimum of 3
 
         aS            deg/rad     optional - Angle at which polygon starts drawing
         aL            deg/rad     optional - Length of polygon from starting angle
         aO            deg/rad     optional - Offset angle at which to drawing the polygon
-        inDegrees     bool        optional - Flag whether angles are passed in as degrees
+        cc            bool        optional - Flag whether to draw counter-clockwise
         closePath     bool        optional - Flag whether to close the path internally
+        inDegrees     bool        optional - Flag whether angles are passed in as degrees
 
     */
 
-    return function (ctx, x, y, r, n, aS, aL, aO, inDegrees, closePath) {
+    return function (ctx, x, y, r, n, aS, aL, aO, cc, closePath, inDegrees) {
 
         var points = [];
         var point, a, j, l, sX, sY;
         var TWO_PI = Math.PI * 2;
-        var FLOATING_POINT_CUTOFF = 0.99;
+        var FP_SENSITIVITY = 0.0001; // Floating point adjustment value
 
         // Validate and set defaults
         n = n >= 3 ? n : 3;                                     // Restrict point count to a minimum of 3
         aS = aS || 0;                                           // Set zero as default starting angle
-        aL = aL || Math.PI * 2;                                 // Set full rotation as default length
+        aL = aL >= 0 ? aL : aL || Math.PI * 2;                  // Set full rotation as default length
         aO = aO || 0;                                           // Set zero as default angle offset
+        cc = cc || false;                                       // Set clockwise as the default direction
         closePath = closePath === undefined ? true : closePath; // Set closed path as the default
 
         // Convert to radians if flagged as degrees
         if ( inDegrees === true ) { aS = aS * ( Math.PI / 180 ); aL = aL * ( Math.PI / 180 ); aO = aO * ( Math.PI / 180 ); }
 
-        // Normalise values to 1 rotation, buffer against floating point artefacts
-        aS = Math.max( ( aS - 0.0000001 ) % TWO_PI, 0 );
-        aL = aL === TWO_PI ? aL : aL % TWO_PI;
+        // Normalise starting angle to nominal angle, buffer against floating point artefacts
+        aS = Math.max( ( aS - FP_SENSITIVITY ) % TWO_PI, 0 );
+        // Restrict length to one full rotation, buffer against floating point artefacts
+        aL = aL < FP_SENSITIVITY ? 0 : Math.min( aL, TWO_PI );
 
         // Find points on circle
         for ( var i = 0; i < n; i++ ) {
@@ -69,10 +68,17 @@ define([
             points.push( point );
         }
 
-        // Draw a full polygon
-        if ( aL >= TWO_PI ) {
+        // Flip canvas if drawing counter clockwise
+        if ( cc ) {
 
-            ctx.beginPath();
+            ctx.translate( x * 2, 0 );
+            ctx.scale( -1, 1 );
+        }
+
+        ctx.beginPath();
+
+        // Draw a full polygon
+        if ( aL >= TWO_PI * ( 1 - FP_SENSITIVITY ) ) {
 
             ctx.moveTo( points[0].x, points[0].y );
 
@@ -83,12 +89,11 @@ define([
 
             ctx.closePath();
         }
-        // Draw a partial polygon
-        else {
+        // Draw a partial polygon (if length is more than zero)
+        else if ( aL > 0 ) {
 
-            ctx.beginPath();
-
-            var startIndex = Math.min( parseInt( aS / ( TWO_PI / n ), 10 ), n - 1 ); // Clamp to avoid floating point errors
+            // var startIndex = Math.min( parseInt( aS / ( TWO_PI / n ), 10 ), n - 1 ); // Clamp to avoid floating point errors
+            var startIndex = parseInt( aS / ( TWO_PI / n ), 10 );
             var startPoint = points[ startIndex ];
             var startProgress = ( aS % ( TWO_PI / n ) ) / ( TWO_PI / n );
             var startNextPoint = points[ ( startIndex + 1 ) % n ];
@@ -108,14 +113,16 @@ define([
             // Draw steps
             for ( j = 1; j <= fullSteps; j++ ) {
 
+                if ( fullSteps > n ) { console.log(aL, fullSteps);}
+
                 nextIndex = ( startIndex + j ) % n;
                 nextPoint = points[ nextIndex ];
 
                 ctx.lineTo( nextPoint.x, nextPoint.y );
             }
 
-            // Draw to end point
-            ctx.lineTo( endPoint.x, endPoint.y );
+            // Draw to end point if at least 1 full step or passing through a point
+            if ( fullSteps > 0 || startIndex !== endIndex ) { ctx.lineTo( endPoint.x, endPoint.y ); }
 
             // Draw tail
             var tailIndex = ( endIndex + 1 ) % n;
