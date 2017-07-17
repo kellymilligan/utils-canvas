c0d23b9ffec4b951e3fa29be6be04ebf33a00bf8
Kelly Milligan <kelly@milligan.net.nz>
2017-07-17 09:11:33 +0100

    Update draw polygon util

--- draw_polygon.js
+++ draw_polygon.js
@@ -1,48 +1,43 @@
 /*
+    Draw a symmetrical polygon on the provided canvas 2d context
 
-    Draw a symmetrical polygon on the provided canvas 2d context.
+    ctx           Context2D     Canvas context to draw to
+    x             Number        X position at centre of circle
+    y             Number        Y position at centre of circle
+    r             Number        Radius of circle
+    n             Number        Number of points that form the polygon, minimum of 3
 
-    ctx           context2D   Canvas context to draw to
-    x             num         X position at centre of circle
-    y             num         Y position at centre of circle
-    r             num         Radius of circle
-    n             num         Number of points that form the polygon, minimum of 3
-
-    aO            deg/rad     optional - Angle of rotation on the polygon
-    inDegrees     bool        optional - Flag whether aO is passed in as degrees
+    theta         Number        optional - Angle of rotation on the polygon in radians or degrees
+    deg           Boolean       optional - Flag whether theta is passed in as degrees
 
 */
 
-import pointOnCircle from '../math/point/point_on_circle';
+import pointOnCircle from '../math/point_on_circle';
 
 export default function (
 
     ctx, x, y, r, n,
 
-    aO = 0,
-    inDegrees = false
+    theta = 0,
+    deg = false
 
 ) {
 
     // Restrict point count to a minimum of 3
-    n = n >= 3 ? n : 3;
-
-    // Set zero as default angle offset
-    aO = aO || 0;
+    n = Math.max( n, 3 );
 
     // Convert to radians if flagged as degrees
-    if ( inDegrees === true ) { aO = ( Math.PI / 180 ) * aO; }
+    if ( deg === true ) { theta = ( Math.PI / 180 ) * theta; }
 
-    var points = [];
-    var point, a;
+    let points = [];
 
     // Find points on circle
-    for ( var i = 0; i < n; i++ ) {
+    for ( let i = 0; i < n; i++ ) {
 
         // Start at -90 degrees to align first point to top
-        a = ( Math.PI * -0.5 ) + aO + ( ( 2 * Math.PI ) / n ) * i;
+        let a = ( Math.PI * -0.5 ) + theta + ( ( 2 * Math.PI ) / n ) * i;
 
-        point = pointOnCircle( x, y, r, a );
+        let point = pointOnCircle( x, y, r, a );
 
         points.push( point.x );
         points.push( point.y );
@@ -52,11 +47,11 @@ export default function (
     ctx.beginPath();
     ctx.moveTo( points[0], points[1] );
 
-    for ( var j = 2, l = points.length; j < l - 1; j += 2 ) {
+    for ( let i = 2, l = points.length; i < l - 1; i += 2 ) {
 
-        ctx.lineTo( points[ j ], points[ j + 1 ] );
+        ctx.lineTo( points[ i ], points[ i + 1 ] );
     }
 
     ctx.closePath();
 
-}
+};
