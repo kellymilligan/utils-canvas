fe0b193f6dc3e0b4ad32c4049e76764bd73fb748
Kelly Milligan <kelly@milligan.net.nz>
2017-07-26 11:15:53 +0100

    Update draw circle util docs

--- draw_circle.js
+++ draw_circle.js
@@ -3,16 +3,16 @@
     Draw a circle on the provided canvas 2d context.
     By default a full, closed-path circle is drawn.
 
-    ctx           context2D   Canvas context to draw to
-    x             num         X position at centre of circle
-    y             num         Y position at centre of circle
-    r             num         Radius of circle
+    ctx         context2D       Canvas context to draw to
+    x           Number          X position at centre of circle
+    y           Number          Y position at centre of circle
+    r           Number          Radius of circle
 
-    c             bool        optional - Close path flag
-    sA            num         optional - Start angle (in Radians)
-    eA            num         optional - End angle (in Radians)
-    aC            bool        optional - Anti-clockwise flag
-    inDegrees     bool        optional - Flag whether angle is passed in as degrees
+    c           Boolean         optional - Close path flag
+    sA          Number          optional - Start angle (in Radians)
+    eA          Number          optional - End angle (in Radians)
+    aC          Boolean         optional - Anti-clockwise flag
+    deg         Boolean         optional - Flag whether angle is passed in as degrees
 
 */
 
@@ -24,13 +24,13 @@ export default function (
     sA = 0,
     eA = Math.PI * 2,
     aC = false,
-    inDegrees = false
+    deg = false
 
 ) {
 
     // Convert to radians if flagged as degrees
-    sA = inDegrees === true ? sA * ( Math.PI / 180 ) : sA;
-    eA = inDegrees === true ? eA * ( Math.PI / 180 ) : eA;
+    sA = deg === true ? sA * ( Math.PI / 180 ) : sA;
+    eA = deg === true ? eA * ( Math.PI / 180 ) : eA;
 
     // Draw circle
     ctx.beginPath();
